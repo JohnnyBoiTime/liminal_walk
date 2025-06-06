281baeadd6d42b03515b98a8c612e7b857fe2387
JohnnyBoiTime <chanky789@icloud.com>
2025-06-06 11:05:34 -0700

    Setup

---
+++ src/App.js
@@ -0,0 +1,106 @@
+import * as THREE from 'three';
+import Controls from './Controls';
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
+
+export default class App {
+  constructor() {
+    this.simulationSpeedMultiplier = 1.0;
+    this._Initialize();
+  }
+
+  _Initialize() {
+    
+    // Create the scene 
+    this.scene = new THREE.Scene();
+
+    // Set up the render, camera, and scene
+    this._SetupRenderer();
+
+    this._SetupCamera();
+
+    this._SetupControls();
+
+    this.inputController = new Controls(this.controls, this.camera);
+
+    // Simulation time
+    this.clock = new THREE.Clock();
+  }
+
+  // Renderers
+  _SetupRenderer() {
+    
+    // Three.js renderer
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.outputEncoding = THREE.sRGBEncoding;
+    this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    this.renderer.toneMappingExposure = 0.25;
+    document.body.appendChild(this.renderer.domElement);
+
+    /*
+    // Label renderer, to label things!
+    this.labelRenderer = new CSS2DRenderer();
+    this.labelRenderer.setSize(window.innerWidth, window.innerHeight);
+    this.labelRenderer.domElement.style.position = 'absolute';
+    this.labelRenderer.domElement.style.top = '0px';
+    this.labelRenderer.domElement.style.pointerEvents = 'none';
+    document.body.appendChild(this.labelRenderer.domElement);
+
+    */
+  }
+
+  // Basic camera setup
+  _SetupCamera() {
+    const aspect = window.innerWidth / window.innerHeight;
+    this.camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 100000);
+    this.camera.position.set(10, 100, 500);
+  }
+
+  /*
+  // Hud stuff
+  _SetUpDomElements() {
+  
+  }
+
+  */
+
+  // Setup for the controls of the simulation
+  _SetupControls() {
+    this.controls = new PointerLockControls(this.camera, this.renderer.domElement);
+    const wrapper = this.controls.getObject();
+    this.scene.add(wrapper);
+    this._RegisterInputEvents();
+    // this._SetUpDomElements();
+  }
+
+  
+  // Have pressing keys do things and resizing camera correctly
+  _RegisterInputEvents() {
+    document.addEventListener('keydown', (e) => this.inputController.handleKeyDown(e));
+    document.addEventListener('keyup',   (e) => this.inputController.handleKeyUp(e));
+    window.addEventListener('resize', () => {
+      const width = window.innerWidth, height = window.innerHeight;
+      this.camera.aspect = width / height;
+      this.camera.updateProjectionMatrix();
+      this.renderer.setSize(width, height);
+      this.labelRenderer.setSize(width, height);
+    })
+  }
+
+  // Starts the simulation
+  start() {
+    this._Animate();
+  }
+
+  // Basic animation loop
+  _Animate() {
+    const delta = this.clock.getDelta();
+
+
+    this.inputController.update(delta, this.camera);
+
+    this.renderer.render(this.scene, this.camera);
+
+    requestAnimationFrame(() => this._Animate());
+  }
+}
---
+++ src/Controls.js
@@ -0,0 +1,98 @@
+import * as THREE from 'three';
+
+// Defines the controls
+export default class Controls {
+
+    // Basic control scheme
+    constructor(controls, camera) {
+        this.controls = controls;
+        this.camera = camera;
+        this.wrapper = controls.getObject();
+
+        this.moving = {
+            forward: false,
+            backward: false,
+            right: false,
+            left: false,
+        };
+
+        this.speed = 500;
+        this.position = 10;
+
+        this.forwardDireciton = new THREE.Vector3();
+        this.rightDirection = new THREE.Vector3();
+    }
+
+    // Pressing down moves camera
+    handleKeyDown(event) {
+        switch(event.code) {
+            case 'KeyW':
+                this.moving.forward = true;
+                break;
+            case 'KeyS':
+                this.moving.backward = true;
+                break;
+            case 'KeyA':
+                this.moving.left = true;
+                break;
+            case 'KeyD':
+                this.moving.right = true;
+                break;               
+        }
+    }
+
+    // Letting go stops moving camera
+    handleKeyUp(event) {
+        switch(event.code) {
+            case 'KeyW':
+                this.moving.forward = false;
+                break;
+            case 'KeyS':
+                this.moving.backward = false;
+                break;
+            case 'KeyA':
+                this.moving.left = false;
+                break;
+            case 'KeyD':
+                this.moving.right = false;
+                break;               
+        }
+    }
+
+    
+
+    // Update direcitons
+    update(delta) {
+
+        // Goes in and out of the scene to explore
+        this.wrapper.getWorldDirection(this.forwardDireciton);
+        this.rightDirection.crossVectors(
+            this.forwardDireciton, this.wrapper.up)
+            .normalize();
+    
+        if (this.moving.forward) {
+            this.wrapper.position.addScaledVector(
+                this.forwardDireciton,
+                this.speed * delta
+            );
+        }
+        if (this.moving.backward) {
+            this.wrapper.position.addScaledVector(
+                this.forwardDireciton,
+                -this.speed * delta
+            );
+        }
+        if (this.moving.left) {
+            this.wrapper.position.addScaledVector(
+                this.rightDirection,
+                -this.speed * delta
+            );
+        }
+        if (this.moving.right) {
+            this.wrapper.position.addScaledVector(
+                this.rightDirection, 
+                this.speed * delta
+            );
+        }
+    }
+ }
